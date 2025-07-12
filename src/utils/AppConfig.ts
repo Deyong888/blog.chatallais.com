e51ba15bb0b5f1267e183b1b12d4613c932cbaa4
Deyong888 <xzywork@163.com>
2025-07-12 23:51:57 +0800

    增加faq及feature模块

--- src/utils/AppConfig.ts
+++ src/utils/AppConfig.ts
@@ -1,8 +1,9 @@
 export const AppConfig = {
-  site_name: 'Astro boilerplate',
-  title: 'Astro boilerplate',
-  description: 'Boilerplate built with Astro using React and Tailwind CSS',
-  author: 'Emma',
-  locale_region: 'en-us',
-  locale: 'en',
+  site_name: 'Astral AI -Your all in one AI',
+  title: '星界AI|一站式AIGC创作平台，让创作更简单',
+  description:
+    '星界AI|一站式AIGC创作平台，集成谷歌Gemini、ChatGPT、Claude、DeepSeek、文心一言、星火、通义、Midjourney等大模型，让创作更简单',
+  author: '麒麟剑',
+  locale_region: 'zh-cn',
+  locale: 'zh',
 };
